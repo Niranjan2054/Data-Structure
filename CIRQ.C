ca165d3d0624b1b4bdbdbcc57a4166b9242a184a
Niranjan2054 <32767223+Niranjan2054@users.noreply.github.com>
2019-05-29 19:35:49 +0545

    queue
    Tail and Head Varying queue
    Tail vary head Fixed Queue
    Circular Queue

---
+++ CIRQ.C
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<conio.h>
+#define MAX 10
+int front =0;
+int rear =0;
+int queue[MAX];
+int isfull(){
+	if((front%MAX==0 && rear%MAX==MAX-1)|| (rear==front-1)){
+		return 1;
+	}
+	return 0;
+}
+int isempty(){
+	if(front==rear){
+		return 1;
+	}
+	return 0;
+}
+void enqueue(int in){
+	if(isfull()){
+		printf("Queue is FULL!");
+	}else{
+		queue[(rear++)%MAX]=in;
+		printf("\nQueued Successfully");
+	}
+}
+int dequeue(){
+	int a;
+	if(isempty()){
+		printf("Queue is EMPTY!");
+		return 0;
+	}
+	a = queue[(front++)%MAX];
+	return a;
+}
+void display(){
+	int i;
+	printf("\n\tQueued Element are: ");
+	for(i=front;i<rear;i++){
+		printf("\t%d",queue[i%MAX]);
+	}
+}
+void main(){
+	int a;
+	while(1){
+		clrscr();
+		printf("\n\t1. Insertion Operation");
+		printf("\n\t2. Deletion Operation");
+		printf("\n\t3. Dislay");
+		printf("\n\t4. Exit");
+		printf("\n\tChoose Option...:");
+		scanf("%d",&a);
+		switch (a){
+			case 1:
+				printf("\nEnter a number to insert: ");
+				scanf("%d",&a);
+				enqueue(a);
+				break;
+			case 2:
+				a = dequeue();
+				if(a){
+					printf("Dequeued Element %d successfully",a);
+				}
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				exit(0);
+				break;
+			default:
+				printf("Choose a valid input");
+		}
+//		printf("front = %d, rear =%d",front%MAX,rear%MAX);
+		getch();
+	}
+}
---
+++ FDTC.C
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<conio.h>
+#define MAX 10
+#define front 0
+int rear =0;
+int queue[MAX];
+int isfull(){
+	if(rear==MAX-1){
+		return 1;
+	}
+	return 0;
+}
+int isempty(){
+	if(front==rear){
+		return 1;
+	}
+	return 0;
+}
+void enqueue(int in){
+	if(isfull()){
+		printf("Queue is FULL!");
+	}else{
+		queue[rear++]=in;
+		printf("\nQueued Successfully");
+	}
+}
+int dequeue(){
+	int a,i;
+	if(isempty()){
+		printf("Queue is EMPTY!");
+		return 0;
+	}
+	a = queue[front];
+	rear--;
+	for(i=0;i<rear;i++){
+		queue[i] = queue[i+1];
+	}
+	return a;
+}
+void display(){
+	int i;
+	printf("\n\tQueued Element are: ");
+	for(i=front;i<rear;i++){
+		printf("\t%d",queue[i]);
+	}
+}
+void main(){
+	int a;
+	while(1){
+		clrscr();
+		printf("\n\t1. Insertion Operation");
+		printf("\n\t2. Deletion Operation");
+		printf("\n\t3. Dislay");
+		printf("\n\t4. Exit");
+		printf("\n\tChoose Option...:");
+		scanf("%d",&a);
+		switch (a){
+			case 1:
+				printf("\nEnter a number to insert: ");
+				scanf("%d",&a);
+				enqueue(a);
+				break;
+			case 2:
+				a = dequeue();
+				if(a){
+					printf("Dequeued Element %d successfully",a);
+				}
+				break;
+			case 3:
+				display();
+				break;
+			case 4:
+				exit(0);
+				break;
+			default:
+				printf("Choose a valid input");
+		}
+		getch();
+	}
+}
